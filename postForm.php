00ff7f8f4c02cfee8e7a963a36b482d0a3cb620b
Camotubi <camotubi@gmail.com>
2017-11-01 23:29:48 -0500

    please work

--- postForm.php
+++ postForm.php
@@ -1,11 +1,12 @@
 <?php 
-if(isset($_POST['submit']))
- 
-{
+
 session_start();
+if(isset($_POST['submit']))
+{
 
 $_SESSION['post-data'] = $_POST;
-
+$_SESSION["persona"] = new Person($_POST["fname"],$_POST["lname"]);
+$_SESSION["cuenta"] = new Cuenta($_POST["plata"]);
 echo '<script type="text/javascript"> window.open("login.php","_self");</script>';
 }
 
